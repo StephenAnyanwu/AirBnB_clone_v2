c13ef17c8749ce682e39563ebe6a79808ec8ff72
Stephen Anyanwu <voltasteve@gmail.com>
2024-09-18 15:37:30 -0700

    task 7

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -2,7 +2,6 @@
 
 """In this module defines DBStorage class"""
 
-
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
@@ -14,6 +13,7 @@ from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
 
+
 class DBStorage:
     """Defines DBStorage class"""
     __engine = None
@@ -21,8 +21,7 @@ class DBStorage:
 
     def __init__(self):
         """
-        
-        """
+         """
         username = os.getenv("HBNB_MYSQL_USER")
         password = os.getenv("HBNB_MYSQL_PWD")
         host = os.getenv("HBNB_MYSQL_HOST")
@@ -89,13 +88,13 @@ class DBStorage:
             Object to be added to the current database session.
         """
         self.__session.add(obj)
-      
+
     def save(self):
         """
         Commit all changes of the current database session (self.__session)
         """
         self.__session.commit()
-      
+
     def delete(self, obj=None):
         """
         Delete from the current database session obj if not None
@@ -106,13 +105,14 @@ class DBStorage:
             Object to be deleted from curret database session.
         """
         self.__session.delete(obj)
-      
+
     def reload(self):
         """
         Create all tables in the database
         """
         Base.metadata.create_all(self.__engine)
-        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        session_factory = sessionmaker(bind=self.__engine,
+                                       expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session()
 
